fed32a77ee56479ba10c80a5f25b85693e6d583c
IndefiniteBen <benpyman@gmail.com>
2024-05-24 18:28:58 +0200

    Clean up comments and test print commands

--- segmentation/tools/convert_okutama_swiss.py
+++ segmentation/tools/convert_okutama_swiss.py
@@ -1,4 +1,3 @@
-# Copyright (c) OpenMMLab. All rights reserved.
 import argparse
 import glob
 import os
@@ -31,8 +30,6 @@ okutama_drone_invert_palette = {v: k for k, v in okutama_drone_palette.items()}
 def imgAnn_convert_from_color(arr_3d, palette=okutama_drone_invert_palette):
     """RGB-color encoding to grayscale labels."""
     arr_2d = np.zeros((arr_3d.shape[0], arr_3d.shape[1]), dtype=np.uint8)
-    # print(f"arr_3d.shape: {arr_3d.shape}")
-    # print(f"arr_2d.shape: {arr_2d.shape}")
     for c, i in palette.items():
         m = np.all(arr_3d == np.array(c).reshape(1, 1, 3), axis=2)
         arr_2d[m] = i
@@ -42,11 +39,8 @@ def imgAnn_convert_from_color(arr_3d, palette=okutama_drone_invert_palette):
 
 def fix_labels(src_path, out_dir, mode):
     label = mmcv.imread(src_path, channel_order='rgb')
-    # print(f"\nlabel.shape: {label.shape}")
     label = imgAnn_convert_from_color(label)
-    # print(f"label.shape: {label.shape}")
     converted_label_img = Image.fromarray(label.astype(np.uint8), mode='P')
-    # print(f"converted_label_img.shape: {converted_label_img.size}")
     converted_label_img.save(osp.join(out_dir, 'ann_dir', mode, osp.basename(src_path)))
                                       
 
@@ -88,23 +82,18 @@ def main():
             img_list = glob.glob(
                 os.path.join(tmp_dir, 'images', dataset_mode, '*.png')) + glob.glob(
                 os.path.join(tmp_dir, 'images', dataset_mode, '*.JPG'))
-            # print('Find the data', img_list)
             src_prog_bar = mmcv.ProgressBar(len(img_list))
             for img in img_list:
                 shutil.copy(img, os.path.join(out_dir, 'img_dir', dataset_mode))
                 src_prog_bar.update()
 
             print('\nProcessing {} annotations...'.format(dataset_mode))
-            # do not skip test as we have annotations for test set! not done!
-            # if dataset_mode != 'test':
             label_list = glob.glob(
                 os.path.join(tmp_dir, 'ground_truth', dataset_mode, '*.png'))
             lab_prog_bar = mmcv.ProgressBar(len(label_list))
             for label in label_list:
                 label = fix_labels(label, out_dir, dataset_mode)
                 lab_prog_bar.update()
-            # for label in label_list:
-            #     shutil.copy(label, os.path.join(out_dir, 'ann_dir', dataset_mode))
 
 
         print('Removing the temporary files...')
